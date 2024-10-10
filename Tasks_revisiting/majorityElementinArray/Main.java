b4add904b5eb530f1d6bba59f71b8bf024f953d7
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-10-10 18:18:40 +0200

    revisited task majorityElementinArray

---
+++ Tasks_revisiting/majorityElementinArray/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+          public static int majorityElement(int[] nums) {
+            Arrays.sort(nums);
+            int count = 0;
+            int n = (int) (nums.length / 2);
+            int majorityElement = 0;
+
+            for(int i = 0; i < nums.length; i++) {
+                for(int j = 1; j < nums.length - 1; j++) {
+                    if(nums[i] == nums[j]) {
+                        count++;
+                    }
+                }
+
+                if(count > n) {
+                    majorityElement = nums[i];
+                }
+            }
+
+            return majorityElement;
+
+        }
+
+        public static void main(String[] args) {
+            int[] arr = { 2, 2, 1, 1, 1, 2, 2 };
+            System.out.println(majorityElement(arr));
+        }
+
+
+}
