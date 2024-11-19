5ac69fa4a7281d2988fe703458ca2ae864503ca5
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-11-19 13:58:49 +0100

    Index of first occurence in a string without using indexOf..

--- src/main/java/org/algorithms/IndexOfFirstOccurrenceInAString.java
+++ src/main/java/org/algorithms/IndexOfFirstOccurrenceInAString.java
@@ -1,18 +1,33 @@
 package org.algorithms;
 
 public class IndexOfFirstOccurrenceInAString {
-    public int strStr(String haystack, String needle) {
-        for(int i = 0; i < haystack.length(); i++) {
-            return haystack.indexOf(needle);
+  public int strStr(String haystack, String needle) {
+    char[] arrHayStack = haystack.toCharArray();
+    char[] arrNeedle = needle.toCharArray();
+
+    for (int i = 0; i <= arrHayStack.length - arrNeedle.length; i++) {
+      int j;
+      for (j = 0; j < arrNeedle.length; j++) {
+        if (arrHayStack[i + j] != arrNeedle[j]) {
+          break;
         }
+      }
 
-        return -1;
+      if (j == arrNeedle.length) {
+        return i;
+      }
     }
+    return -1;
+  }
 
-    public static void main(String[] args) {
-        IndexOfFirstOccurrenceInAString test = new IndexOfFirstOccurrenceInAString();
-        String string1 = "sadbutsad";
-        String string2 = "sad";
-        System.out.println(test.strStr(string1, string2));
-    }
+  public static void main(String[] args) {
+    IndexOfFirstOccurrenceInAString test = new IndexOfFirstOccurrenceInAString();
+    String string1 = "sadbutsad";
+    String string2 = "sad";
+    System.out.println(test.strStr(string1, string2));
+
+    String string3 = "freecodecamp";
+    String string4 = "code";
+    System.out.println(test.strStr(string3, string4));
+  }
 }
