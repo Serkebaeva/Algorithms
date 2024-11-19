68bdfaefee9827b07b706cb1f03e44786fcf81b5
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-11-19 13:30:19 +0100

    Palindrome number without converting into String

--- src/main/java/org/algorithms/PalindromeNumber.java
+++ src/main/java/org/algorithms/PalindromeNumber.java
@@ -1,21 +1,27 @@
 package org.algorithms;
 
 public class PalindromeNumber {
-    public boolean isPalindrome(int x) {
-        StringBuilder sb = new StringBuilder();
-        String str = String.valueOf(x);
-        for(int i = str.length()-1; i >= 0; i--) {
-            sb.append(str.charAt(i));
-        }
+  public boolean isPalindrome(int x) {
+    if (x < 0) return false;
 
-        String strBackward = sb.toString();
-        return str.equals(strBackward);
+    int givenNumber = x;
+    int reversedNumber = 0;
+
+    while (x != 0) {
+      int digit = x % 10;
+      reversedNumber = reversedNumber * 10 + digit;
+      x /= 10;
     }
 
-    public static void main(String[] args) {
-        PalindromeNumber str = new PalindromeNumber();
-        System.out.println(str.isPalindrome(123));
-        System.out.println(str.isPalindrome(121));
-        System.out.println(str.isPalindrome(-121));
-    }
+    return givenNumber == reversedNumber;
+  }
+
+  public static void main(String[] args) {
+    PalindromeNumber str = new PalindromeNumber();
+    System.out.println(str.isPalindrome(123));
+    System.out.println(str.isPalindrome(121));
+    System.out.println(str.isPalindrome(-121));
+    System.out.println(str.isPalindrome(21));
+    System.out.println(str.isPalindrome(11));
+  }
 }
