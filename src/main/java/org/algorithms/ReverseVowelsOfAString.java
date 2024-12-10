d3c86b60e8b55579e5861a2d4997ee7338e533d4
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-12-10 12:42:36 +0100

    Added ReverseVowelsOfAString.java

---
+++ src/main/java/org/algorithms/ReverseVowelsOfAString.java
@@ -0,0 +1,42 @@
+package org.algorithms;
+
+public class ReverseVowelsOfAString {
+  public String reverseVowels(String s) {
+    StringBuilder sb = new StringBuilder(s);
+    int start = 0;
+    int end = s.length() - 1;
+
+    while (start < end) {
+      // Find the next vowel from the start
+      if (isVowel(sb.charAt(start))) {
+        // Find the next vowel from the end
+        while (start < end && !isVowel(sb.charAt(end))) {
+          end--;
+        }
+
+        // Swap the vowels
+        char temp = sb.charAt(start);
+        sb.setCharAt(start, sb.charAt(end));
+        sb.setCharAt(end, temp);
+
+        // Move pointers after swapping
+        start++;
+        end--;
+      } else {
+        // Move start pointer forward if not a vowel
+        start++;
+      }
+    }
+
+    return sb.toString();
+  }
+
+  private boolean isVowel(char c) {
+    return "aeiouAEIOU".indexOf(c) != -1;
+  }
+
+  public static void main(String[] args) {
+    ReverseVowelsOfAString test = new ReverseVowelsOfAString();
+    System.out.println(test.reverseVowels("CreAtive")); // Expected output: "CreitAve"
+  }
+}
