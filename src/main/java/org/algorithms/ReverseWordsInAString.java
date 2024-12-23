a0219d80df9e31a795b1c98d420aa0a2b49b9447
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-12-23 22:24:43 +0100

    Added reverseWords() method inside a ReverseWordsInAString.java

---
+++ src/main/java/org/algorithms/ReverseWordsInAString.java
@@ -0,0 +1,25 @@
+package org.algorithms;
+
+public class ReverseWordsInA_String {
+  public String reverseWords(String s) {
+    String[] sArray = s.split(" ");
+    StringBuilder sb = new StringBuilder();
+
+    for (String word : sArray) {
+      for (int i = word.length() - 1; i >= 0; i--) {
+        sb.append(word.charAt(i));
+      }
+      sb.append(" ");
+    }
+
+    return sb.toString().trim();
+  }
+
+  public static void main(String[] args) {
+    ReverseWordsInA_String test = new ReverseWordsInA_String();
+    System.out.println(
+        test.reverseWords(
+            "Let's take LeetCode contest")); // Expected output: "s'teL ekat edoCteeL tsetnoc"
+    System.out.println(test.reverseWords("Mr Ding")); // Expected output: ""rM gniD""
+  }
+}
