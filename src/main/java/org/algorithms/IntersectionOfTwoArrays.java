2d632050a9791fdbc7b9869fdcae121218d6f825
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-12-25 16:20:26 +0100

    Removed redundant jump

--- src/main/java/org/algorithms/IntersectionOfTwoArrays.java
+++ src/main/java/org/algorithms/IntersectionOfTwoArrays.java
@@ -16,14 +16,16 @@ class IntersectionOfTwoArrays {
 
     for (int i = 0; i < nums1.length; i++) {
       for (int j = 0; j < nums2.length; j++) {
-        if ((nums1[i]) != (nums2[j])) {
-          continue;
-        } else {
+        if ((nums1[i]) == (nums2[j])) {
           numbers.add(nums2[j]);
         }
       }
     }
+
+    // Log the result
     logger.info(() -> String.format("%s", numbers));
+
+    // Convert the set to an array
     int[] arr = new int[numbers.size()];
     int n = 0;
     for (int num : numbers) {
